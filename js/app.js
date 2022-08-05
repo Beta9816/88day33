b0efe05066bb823b887c24d0bd2e5327a23240a6
Beta9816 <107175845+Beta9816@users.noreply.github.com>
2022-08-05 12:59:07 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/88day+.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/88day++.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
